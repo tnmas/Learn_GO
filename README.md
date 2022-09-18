8ec1e472584b2c7f938b6c4908360d24f017db98
Tsigereda Nebai Kidane <tsigereda.nebai@ost.ch>
2022-09-18 14:37:52 +0200

    Learn datatypes

--- README.md
+++ README.md
@@ -13,3 +13,8 @@
 - Initialize the project with the name of the project
 - Use Go packages to build the project
 
+## Start learning the basics
+- Variables: using var for normal variables and constant variables
+- fmt: Println, Printf, Print
+- Data types: string, integer, float, struct, array, map
+
--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func test(name string)  {
-	fmt.Println("Hello", name)
+func userDetails(firstName string, lastName string, age uint, jobType string) {
+	fmt.Printf("\nYour Fullname: %v %v \n", firstName, lastName)
+	fmt.Printf("Age: %v \n", age)
+	fmt.Printf("Your Job: %v \n", jobType)
+
 }
 
 func main() {
-	yourName := "John Smith"
-	test(yourName)
+	var firstName string
+	var lastName string
+	var age uint
+	var jobType string
+
+	fmt.Println("Enter your Firstname: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your Lastname: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your age: ")
+	fmt.Scan(&age)
+	fmt.Println("Enter your job: ")
+	fmt.Scan(&jobType)
+
+	userDetails(firstName, lastName, age, jobType)
 }
